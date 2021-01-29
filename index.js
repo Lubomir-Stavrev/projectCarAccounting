5b52418f1eebd12d36c8d98a275814075ba37ddd
Lubomir Stavrev <62726686+Lubomir-Stavrev@users.noreply.github.com>
2021-01-29 12:59:38 +0200

    adding view engine

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const config = require('./config/config');
-
+const expressConfig = require('./config/express');
 const app = express();
+
+expressConfig(app);
 
 app.listen(config.PORT, () => console.log(`Server is running on port: ${config.PORT}...`))
