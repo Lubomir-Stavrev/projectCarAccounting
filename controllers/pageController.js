c08b3d2c8a7d01024293831b80a5d085927e6554
Lubomir Stavrev <62726686+Lubomir-Stavrev@users.noreply.github.com>
2021-01-31 22:02:58 +0200

    adding details car render

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -5,14 +5,22 @@ const pageService = require('../services/pageService');
 router.get('/', (req, res) => {
     pageService.getAll().then(result => {
 
+        res.render('home', { title: 'Home', cars: result });
         console.log(result);
     })
-    res.render('home', { title: 'Home' });
 })
 
 router.get('/create', (req, res) => {
 
     res.render('create', { title: 'Create' });
+})
+
+router.get('/details/:id', async(req, res) => {
+
+    let car = await pageService.getOne(req.params.id);
+    console.log(car);
+    res.render('details', { title: 'details', car })
+
 })
 
 router.post('/create', (req, res) => {
